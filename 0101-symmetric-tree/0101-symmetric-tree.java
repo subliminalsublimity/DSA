c96101a2498bc34f37c6716b99b2d3f626f9876b
subliminalsublimity <133903074+subliminalsublimity@users.noreply.github.com>
2025-08-29 01:54:31 +0530

    Time: 0 ms (100%), Space: 41.7 MB (84.04%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -19,13 +19,12 @@ class Solution {
         
     }
 
-    public boolean isSymmetricHelp(TreeNode left , TreeNode right ){
-        if(left == null || right == null){
-            return left == right;
-        }
+    public boolean isSymmetricHelp(TreeNode left , TreeNode right){
+        if(left == null || right == null) return left == right;
 
-        if(left.val != right.val) return false;
+        if(left.val != right.val ) return false;
 
         return isSymmetricHelp(left.left , right.right) && isSymmetricHelp(left.right , right.left);
+        
     }
 }
