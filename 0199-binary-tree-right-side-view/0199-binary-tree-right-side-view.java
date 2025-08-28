1d0d91c3e7d9cfaf408db7e95a138df400769b78
subliminalsublimity <133903074+subliminalsublimity@users.noreply.github.com>
2025-08-28 22:19:39 +0530

    Time: 0 ms (100%), Space: 42 MB (92%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,20 +15,18 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        ArrayList<Integer> ans = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
         rightView(root , ans , 0);
         return ans;
     }
 
-    public void rightView(TreeNode curr , List<Integer> ans , int currDepth){
-
+    public void rightView (TreeNode curr , List<Integer> ans , int currDepth){
         if(curr == null) return;
-
         if(currDepth == ans.size()){
             ans.add(curr.val);
         }
 
         rightView(curr.right , ans , currDepth + 1);
-        rightView(curr.left ,  ans , currDepth + 1);
+        rightView(curr.left , ans , currDepth + 1);
     }
 }
