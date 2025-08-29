add4f112e61ebdc04609f1944b9fa91b241c1f14
subliminalsublimity <133903074+subliminalsublimity@users.noreply.github.com>
2025-08-29 22:06:00 +0530

    Time: 7 ms (60.1%), Space: 44.9 MB (34.85%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,42 +8,18 @@
  * }
  */
 class Solution {
-    private boolean getPath(TreeNode root , ArrayList<Integer> arr , int x){
-        if(root == null) return false;
-        arr.add(root.val);
-
-        if(root.val == x) return true;
-
-        if(getPath(root.left , arr , x) || getPath(root.right , arr ,x)){
-            return true;
-        }
-        arr.remove(arr.size() - 1);
-        return false;
-    }
-
-    public ArrayList<Integer> solve(TreeNode A , int B){
-        ArrayList<Integer> arr = new ArrayList<>();
-        if(A == null) return arr;
-        getPath(A, arr , B);
-        return arr;
-    }
-
-
-    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
-        ArrayList<Integer> getPath1 = new ArrayList<>();
-        ArrayList<Integer> getPath2 = new ArrayList<>();
+        if(root == null || root == p || root == q) return root;
 
+        TreeNode left = lowestCommonAncestor(root.left , p , q );
+        TreeNode right = lowestCommonAncestor(root.right , p , q);
 
-        getPath(root , getPath1 , p.val);
-        getPath(root , getPath2 , q.val);
-
-        int i = 0;
-        while(i<getPath1.size() && i < getPath2.size()){
-            if(!getPath1.get(i).equals(getPath2.get(i))) break;
-            i++;
+        if(left == null) return right;
+        else if(right == null) return left;
+        else{
+            return root;
         }
-        return new TreeNode(getPath1.get(i - 1));
+        
     }
 }
