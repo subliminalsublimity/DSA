a3596755e166324c751b2d6f7511f88fbb867a57
subliminalsublimity <133903074+subliminalsublimity@users.noreply.github.com>
2025-08-31 02:01:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,22 +1,22 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+
         int n = nums.length;
-         k = k % n;
 
-         int [] temp = new int [k];
+        int [] temp = new int [k];
+        k = k % n ;
 
-         for(int i=0 ; i<k ;i++){
+        for(int i=0; i<k ; i++){
             temp[i] = nums[n-k+i];
-         }
+        }
 
-         for(int i = n - 1 ; i>=k ;i--){
-            nums[i] = nums[i-k];
+        for(int i= n - 1 ; i>=k ; i--){
+            nums[i] = nums[n - k]; 
+        }
 
-         }
-
-         for(int i=0 ; i<k ; i++){
+        for(int i=0 ; i<k ; i++){
             nums[i] = temp[i];
-         }
+        }
         
     }
 }
