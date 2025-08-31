8f05288d3ad444d4a86184c1cb80a911fb5d1fe9
subliminalsublimity <133903074+subliminalsublimity@users.noreply.github.com>
2025-08-31 19:16:29 +0530

    Time: 1 ms (99.63%), Space: 46.2 MB (54.22%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+
+        int [] temp = new int [n];
+
+        int idx = 0;
+
+        for(int i= 0 ; i<n ; i++){
+            if(nums[i] != 0){
+                temp[idx++] = nums[i];
+            }
+        }
+
+        for(int i=0 ; i < n ; i++){
+            nums[i] = temp[i];
+        }
+        
+    }
+}
