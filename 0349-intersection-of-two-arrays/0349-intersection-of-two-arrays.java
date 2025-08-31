7c7375c7c182c2898cacb14e12cb3868acf9d0c0
subliminalsublimity <133903074+subliminalsublimity@users.noreply.github.com>
2025-08-31 20:21:00 +0530

    Time: 2 ms (96.75%), Space: 43.6 MB (35.02%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        Set<Integer> st = new HashSet<>();
+
+        for(int i=0 ; i<nums1.length ; i++){
+            st.add(nums1[i]);
+        }
+
+          Set<Integer> resultSet = new HashSet<>();
+        for(int i=0 ; i<nums2.length ; i++){
+            if(st.contains(nums2[i])) {
+                resultSet.add(nums2[i]);
+            }
+        }
+
+        int [] res = new int[resultSet.size()] ;
+        int idx = 0;
+
+        for(int num : resultSet){
+            res[idx++] = num;
+        } 
+
+        return res;   
+    }
+}
