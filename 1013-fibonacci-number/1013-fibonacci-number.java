e8fcd389824fa38427f0e8f5cd0fd9cbd261067f
subliminalsublimity <133903074+subliminalsublimity@users.noreply.github.com>
2025-09-02 00:47:33 +0530

    Time: 8 ms (45.45%), Space: 40.3 MB (64.24%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int fib(int n) {
+
+        if(n<=1) return n;
+       int last = fib(n-1);
+       int secondLast = fib(n-2);
+
+        return last + secondLast;
+        
+    }
+}
