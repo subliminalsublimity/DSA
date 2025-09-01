55da21379a64b21809c7bf080d257855d648e26a
subliminalsublimity <133903074+subliminalsublimity@users.noreply.github.com>
2025-09-02 03:17:39 +0530

    Time: 1 ms (100%), Space: 42.4 MB (80.59%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,46 +1,36 @@
 class Solution {
     public int myAtoi(String s) {
 
-
-  
-       
         s = s.trim();
-        if (s.isEmpty()) return 0;   
+        if(s.isEmpty()) return 0 ;
 
-        int index = 0;
-        int sign = 1;
+        int idx = 0 , sign = 1 ;
 
-        
-        if (s.charAt(index) == '-') {
+        if(s.charAt(0) == '-'){
             sign = -1;
-            index++;
-        } else if (s.charAt(index) == '+') {
-            index++;
+            idx ++;
         }
 
+        else if(s.charAt(idx) == '+'){
+            idx++;
+        }
+
+        return convert(s , idx , sign , 0 );
+
         
-        return convert(s, index, sign, 0);
     }
 
-    private int convert(String s, int index, int sign, long number) {
-        
-        if (index >= s.length() || !Character.isDigit(s.charAt(index))) {
-            return (int)(sign * number);
+    private int convert(String s , int idx , int sign , long number){
+        if(idx >= s.length() || !Character.isDigit(s.charAt(idx))){
+            return (int) (sign * number);
         }
 
-        
-        int digit = s.charAt(index) - '0'; 
+        int digit = s.charAt(idx) - '0';
         number = number * 10 + digit;
 
-       
-        if (sign == 1 && number > Integer.MAX_VALUE) {
-            return Integer.MAX_VALUE;
-        }
-        if (sign == -1 && -number < Integer.MIN_VALUE) {
-            return Integer.MIN_VALUE;
-        }
+        if(sign == 1 && number > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if(sign == -1 && -number < Integer.MIN_VALUE) return Integer.MIN_VALUE;
 
-       
-        return convert(s, index + 1, sign, number);
+        return convert(s , idx + 1 , sign , number);
     }
 }
