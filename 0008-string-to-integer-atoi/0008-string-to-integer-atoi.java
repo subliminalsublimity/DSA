9f918d02b23ee9e687696495f1ae14d1cb3e173d
subliminalsublimity <133903074+subliminalsublimity@users.noreply.github.com>
2025-09-02 02:46:00 +0530

    Time: 1 ms (100%), Space: 42.1 MB (98.65%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int myAtoi(String s) {
+
+
+  
+       
+        s = s.trim();
+        if (s.isEmpty()) return 0;   
+
+        int index = 0;
+        int sign = 1;
+
+        
+        if (s.charAt(index) == '-') {
+            sign = -1;
+            index++;
+        } else if (s.charAt(index) == '+') {
+            index++;
+        }
+
+        
+        return convert(s, index, sign, 0);
+    }
+
+    private int convert(String s, int index, int sign, long number) {
+        
+        if (index >= s.length() || !Character.isDigit(s.charAt(index))) {
+            return (int)(sign * number);
+        }
+
+        
+        int digit = s.charAt(index) - '0';
+        number = number * 10 + digit;
+
+       
+        if (sign == 1 && number > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        if (sign == -1 && -number < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+
+       
+        return convert(s, index + 1, sign, number);
+    }
+}
