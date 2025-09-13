0816bacb8fd3c313c93c0f43494e5df66cbeaa01
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-09-13 21:11:27 +0530

    Time: 2 ms (45.79%), Space: 43 MB (82.17%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        if(n <= 0) return res;
+        char [] curr = new char[2 * n];
+        generateAll(curr , 0 , res);
+        return res;
+    }
+
+    private void generateAll(char [] curr , int pos , List<String> res){
+        if(pos == curr.length){
+            if(isValid(curr)){
+                res.add(new String(curr));
+            }
+            return;
+        }
+
+        curr[pos] = '(';
+        generateAll(curr , pos + 1 , res);
+
+        curr[pos] = ')';
+        generateAll(curr , pos + 1 , res);
+    }
+
+    private boolean isValid(char[] arr){
+        int balance = 0;
+        for(char c: arr){
+            if(c == '(') balance++;
+            else balance --;
+         if(balance < 0) return false;
+        }
+        return balance == 0;
+    }
+}
