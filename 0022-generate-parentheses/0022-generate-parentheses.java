11d2a2f1038d1a9ad8a3bfc5611eb5e000b36b05
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-09-15 23:09:29 +0530

    Time: 1 ms (81.19%), Space: 43.4 MB (45.36%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-         List<String> res = new ArrayList<String>();
-        recurse(res, 0, 0, "", n);
+        List<String> res = new ArrayList<>();
+        generateAll(res , 0 , 0 , "" ,n );
         return res;
     }
-    
-    public void recurse(List<String> res, int left, int right, String s, int n) {
-        if (s.length() == n * 2) {
+
+    private void generateAll(List<String> res , int left , int right , String s , int n){
+        if(s.length() == 2 * n){
             res.add(s);
             return;
         }
-        
-        if (left < n) {
-            recurse(res, left + 1, right, s + "(", n);
+
+        if(left < n){
+            generateAll(res , left + 1 , right , s + "(" , n);
         }
-        
-        if (right < left) {
-            recurse(res, left, right + 1, s + ")", n);
+
+        if(right <  left){
+            generateAll(res , left , right + 1 , s + ")" , n);
         }
     }
 }
