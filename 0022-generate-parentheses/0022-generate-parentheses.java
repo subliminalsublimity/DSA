0064c0386af052dc2e548b132848e75922b777b8
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-09-15 22:21:25 +0530

    Time: 2 ms (45.68%), Space: 42.5 MB (99.12%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -2,12 +2,12 @@ class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
         if(n <= 0) return res;
-        char [] curr = new char[2 * n];
+        char [] curr = new char [2 * n];
         generateAll(curr , 0 , res);
         return res;
     }
 
-    private void generateAll(char [] curr , int pos , List<String> res){
+    private void generateAll(char [] curr ,int pos , List<String> res){
         if(pos == curr.length){
             if(isValid(curr)){
                 res.add(new String(curr));
@@ -16,19 +16,20 @@ class Solution {
         }
 
         curr[pos] = '(';
-        generateAll(curr , pos + 1 , res);
+        generateAll(curr , pos + 1, res);
 
         curr[pos] = ')';
-        generateAll(curr , pos + 1 , res);
+        generateAll(curr , pos + 1, res);
     }
 
-    private boolean isValid(char[] arr){
-        int balance = 0;
-        for(char c: arr){
-            if(c == '(') balance++;
-            else balance --;
-         if(balance < 0) return false;
+
+        private  boolean isValid(char[] arr){
+            int balance = 0;
+            for(char c : arr){
+                if(c == '(') balance++;
+                else balance--;
+                if(balance < 0) return false; 
+            }
+    return balance == 0;
         }
-        return balance == 0;
-    }
 }
