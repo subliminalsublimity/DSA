13e911c0f9da8ea310281bbcf43456eb23285423
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-09-18 20:23:54 +0530

    Time: 3 ms (72.23%), Space: 43.9 MB (5.81%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -2,24 +2,23 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(candidates);
-        allCombinations(0 , candidates , target , res , new ArrayList<>());
+        allCombinations(0 , candidates , target , res ,new ArrayList<>());
         return res;
-        
     }
 
     private void allCombinations(int ind , int[] arr , int target , List<List<Integer>> res , List<Integer> ds){
-        if(target == 0) {
-            res.add(new ArrayList<>(ds));
-            return ; 
-        }
+        if(target == 0){
+         res.add(new ArrayList<>(ds));
+        return;
+    }
 
-        for(int i=ind ; i<arr.length ; i++){
-            if(i > ind && arr[i] == arr[i-1]) continue;
-            if(arr[i]  > target) break;
+    for(int i = ind ; i< arr.length ; i++){
+        if(i > ind && arr[i] == arr[i-1]) continue;
+        if(arr[i] >  target) break;
 
-            ds.add(arr[i]);
-            allCombinations(i+1 , arr , target - arr[i] , res ,ds);
-            ds.remove(ds.size() - 1);
-        }
+        ds.add(arr[i]);
+        allCombinations(i + 1 , arr, target - arr[i] , res , ds );
+        ds.remove(ds.size() - 1);
+    }
     }
 }
