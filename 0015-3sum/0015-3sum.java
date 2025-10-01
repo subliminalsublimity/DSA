f9523feeead23f07353e0f539c9597bb8f44f2bc
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-10-02 00:51:05 +0530

    Time: 2424 ms (5.01%), Space: 53.5 MB (8.1%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
-        List<List<Integer>> res = new ArrayList<>();
+        Set<List<Integer>> st = new HashSet<>();
         Arrays.sort(nums);
 
-        for(int i=0 ; i<n ; i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
-            int j = i+1;
-            int k = n-1;
+        for(int i=0 ; i<n ;i++){
+            Set<Integer> hash = new HashSet<>();
+            for(int j= i+1 ; j<n ; j++){
+                int third = -(nums[i] +  nums[j]);
+                if(hash.contains(third)){
+                    List<Integer> triplet = Arrays.asList(nums[i] , nums[j] , third);
+                    Collections.sort(triplet);
+                    st.add(triplet);
+                }
+                hash.add(nums[j]);
 
-            while(j<k){
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if(sum>0){
-                    k--;
-                }
-                else if(sum<0){
-                    j++;
-                }
-                else{
-                    List<Integer> triplet = Arrays.asList(nums[i],nums[j],nums[k]);
-                    res.add(triplet);
-                    j++;
-                    k--;
-                    while(j<k && nums[j] == nums[j-1]) j++;
-                    while(j<k && nums[k] == nums[k+1]) k--;
-                }
             }
         }
-        return res;
+        return new ArrayList<>(st);
         
     }
 }
