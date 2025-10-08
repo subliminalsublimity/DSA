a6101ef1a41819e1fb3428e053734d348807b85e
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-10-08 14:40:06 +0530

    Time: 0 ms (100%), Space: 40.8 MB (91.77%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,19 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0];  // only one house
-        if (n == 2) return Math.max(nums[0], nums[1]);  // two houses
+        if (n == 1) return nums[0];  
+        if (n == 2) return Math.max(nums[0], nums[1]);  
 
-        // Case 1: loot from 0 to n-2
+ 
         int case1 = robLinear(nums, 0, n - 2);
 
-        // Case 2: loot from 1 to n-1
         int case2 = robLinear(nums, 1, n - 1);
 
         return Math.max(case1, case2);
     }
 
-    // Space-optimized linear robber logic (same as your House Robber I)
     private int robLinear(int[] nums, int start, int end) {
         int prev = nums[start];
         int prev2 = 0;
