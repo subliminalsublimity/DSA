6a836829f144cd8f84e602f067b8ffc07097c69f
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-10-08 14:18:51 +0530

    Time: 0 ms (100%), Space: 41 MB (81.26%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,27 +1,29 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length ;
-        if(n == 1) return nums[0];
+        int n = nums.length;
+        if(n == 1 ) return nums[0];
         if(n == 2) return Math.max(nums[0] , nums[1]);
-       int firstHouse = helper(n-2 , nums , 0);
-       int lastHouse = helper(n-1 , nums , 1 );
 
-       return Math.max(firstHouse , lastHouse);
+
+        int firstHouse = helper(n-2 , nums , 0);
+        int lastHouse = helper(n-1 , nums , 1);
+
+        return Math.max(firstHouse , lastHouse);
     }
 
-    public int helper( int end , int [] nums , int start ){
+
+    public int helper(int end , int [] nums , int start ){
         int n = end - start + 1;
-        if( n == 1 ) return nums[start];
-         int [] dp = new int [n];
-        dp[0] = nums[start];
-        dp[1]= Math.max(nums[start] , nums[start+1]);
+        if(n == 1) return nums[start];
+        int prev2 = nums[start] ;
+        int prev = Math.max(nums[start] , nums[start + 1]) ;
+        int curr = prev;
 
-
-        for(int i= 2; i<n ; i++){
-            int pick = nums[i + start] + dp[i-2];
-            int notPick = dp[i-1];
-            dp[i] = Math.max(pick , notPick);
+        for(int i=2 ; i<n ; i++){
+            curr = Math.max(prev2 + nums[start + i] ,  prev);
+            prev2 = prev;
+            prev = curr;
         }
-    return dp[n-1];
+        return curr;
     }
 }
