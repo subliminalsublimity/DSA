c9b85855183af9e37e3ed22e4474f85f1e0e5832
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-10-13 00:47:06 +0530

    Time: 0 ms (100%), Space: 40.5 MB (62.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int [][] dp = new int [m][n];
-        for(int i= 0 ; i<m ; i++){
-            for(int j = 0 ; j<n ; j++ ){
-                dp[i][j] = -1;
-            }
+
+    
+
+        for(int i=0 ; i<m;i++){
+            dp[i][0] = 1;
         }
-        return f( 0 , 0 , m , n , dp);
-    }
+            for(int j = 0 ; j<n ; j++){
+                dp[0][j] = 1;
+            }
 
-    private int f(int i ,int j , int m , int n , int [][]dp ){
-        if(i == m - 1 && j == n -1 )  return 1;
-        if(i>= m || j >= n) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int right = f(i , j+1 , m , n , dp); 
-        int down = f(i+1 , j , m , n , dp);
-
-        dp[i][j] =  right + down;
-        return dp[i][j];
+            for(int i=1 ; i<m; i++){
+                for(int j=1 ; j<n; j++){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];  //up +  left
+                }
+            }
+            return dp[m-1][n-1];
     }
 }
