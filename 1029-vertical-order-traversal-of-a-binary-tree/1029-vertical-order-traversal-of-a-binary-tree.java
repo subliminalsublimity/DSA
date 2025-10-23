38da58e0c61860968fdfcdd5650affb4b8fa6ed5
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-10-23 20:25:08 +0530

    Time: 3 ms (79.14%), Space: 43.1 MB (7.98%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -14,70 +14,62 @@
  * }
  */
 
- class Tuple {
-    TreeNode node; 
+ class Tuple{
+    TreeNode node;
     int row;
     int col;
- 
 
- public  Tuple (TreeNode _node , int _row , int _col){
-    node = _node;
-    row = _row;
-    col = _col;
+    public Tuple(TreeNode _node , int _row , int _col){
+        node = _node;
+        row = _row;
+        col = _col;
+    }
  }
- }
+
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
+        TreeMap<Integer , TreeMap<Integer , PriorityQueue<Integer>>> mpp = new TreeMap<>();
         
-     TreeMap<Integer , TreeMap <Integer , PriorityQueue<Integer>>> mpp = new TreeMap<>();
-     Queue<Tuple> que = new LinkedList<>();
-     que.offer(new Tuple (root , 0 , 0));
+        Queue<Tuple> que = new LinkedList<>();
+        que.offer(new Tuple(root , 0 , 0));
+
+        while(!que.isEmpty()){
+            Tuple tuple = que.poll();
+            TreeNode node = tuple.node;
+            int x = tuple.row;
+            int y = tuple.col;
+
+            if(!mpp.containsKey(x)){
+                mpp.put(x , new TreeMap<>());
+            }
+
+            if(!mpp.get(x).containsKey(y)){
+                mpp.get(x).put(y , new PriorityQueue<>());
+            }
+            mpp.get(x).get(y).offer(node.val);
 
 
-     while(!que.isEmpty()){
-        Tuple tuple = que.poll();
-        TreeNode node = tuple.node;
-        int x = tuple.row;
-        int y = tuple.col;
+            if(node.left != null){
+                que.offer(new Tuple(node.left , x-1 , y+1));
+            }
 
-
-        if(!mpp.containsKey(x)){
-            mpp.put(x, new TreeMap<>());
-        }
-
-        if(!mpp.get(x).containsKey(y)){
-            mpp.get(x).put(y , new PriorityQueue<>());
-
-        }
-
-        mpp.get(x).get(y).offer(node.val);
-
-
-        if(node.left != null){
-            que.offer(new Tuple(node.left , x-1 , y+1));
-        }
-
-        if(node.right != null){
-            que.offer(new Tuple(node.right , x+1 , y+1 ));
-        }
-     }
-
-
-     List<List<Integer>> ls = new ArrayList<>();
-     for(TreeMap<Integer , PriorityQueue<Integer>> ys : mpp.values()){
-        List<Integer> colList = new ArrayList<>();
-
-        for(PriorityQueue<Integer> nodes : ys.values()){
-            while(!nodes.isEmpty()){
-                colList.add(nodes.poll());
+            if(node.right != null){
+                que.offer(new Tuple(node.right , x+1 , y+1));
             }
         }
-        ls.add(colList);
-     }
 
-     return ls;
+            List<List<Integer>> ls = new ArrayList<>();
+
+            for(TreeMap<Integer,PriorityQueue<Integer>> ys: mpp.values()){
+                List<Integer> colList = new ArrayList<>();
+
+                for(PriorityQueue<Integer> nodes : ys.values()){
+                    while(!nodes.isEmpty()){
+                        colList.add(nodes.poll());
+                    }
+                }
+                ls.add(colList);
+        }
+            return ls;
     }
 }
-
-
-    
