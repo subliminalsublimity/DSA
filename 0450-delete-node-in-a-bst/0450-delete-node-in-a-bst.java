514f6efa591def467dddbefbc4e52173847e2adf
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-10-26 13:08:09 +0530

    Time: 0 ms (100%), Space: 46 MB (78.64%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -15,15 +15,11 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null){
-            return null;
-        }
-
-        if(root.val == key){
-            return helper(root);
-        }
+        if(root == null) return null;
+        if(root.val == key) return helper(root);
 
         TreeNode dummy = root;
+
         while(root != null){
             if(root.val > key){
                 if(root.left != null && root.left.val == key){
@@ -34,7 +30,6 @@ class Solution {
                     root = root.left;
                 }
             }
-
             else{
                 if(root.right != null && root.right.val == key){
                     root.right = helper(root.right);
@@ -48,14 +43,11 @@ class Solution {
         return dummy;
     }
 
-    public TreeNode helper(TreeNode root){
-        if(root.left == null){
-            return root.right;
-        }
 
-        else if(root.right == null){
-            return root.left;
-        }
+
+    public TreeNode helper(TreeNode root){
+        if(root.left == null) return root.right;
+        if(root.right == null) return root.left;
 
         else{
             TreeNode rightChild = root.right;
@@ -65,10 +57,9 @@ class Solution {
         }
     }
 
+
     public TreeNode findLastRight(TreeNode root){
-        if(root.right == null){
-            return root;
-        }
+        if(root.right == null) return root;
         return findLastRight(root.right);
     }
 }
