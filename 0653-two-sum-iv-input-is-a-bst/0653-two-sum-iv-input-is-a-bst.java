a2061f871c362f3df04a2101e828a6dde7eecf3f
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-10-27 15:05:30 +0530

    Time: 2 ms (97%), Space: 45.5 MB (19.92%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -13,55 +13,58 @@
  *     }
  * }
  */
+
+
+ public class BSTIterator {
+    private Stack<TreeNode> st = new Stack<>();
+    boolean reverse = true;
+
+    public BSTIterator(TreeNode root, boolean isReverse){
+        reverse = isReverse;
+        pushAll(root);
+    }
+
+    public boolean hasNext(){
+        return !st.isEmpty();
+    }
+
+    public int next(){
+        TreeNode temp = st.pop();
+        if(reverse == false) pushAll(temp.right);
+        else pushAll(temp.left);
+        return temp.val;
+    }
+
+    private void pushAll(TreeNode node){
+        while(node != null){
+            st.push(node);
+            if(reverse == true){
+                node = node.right;
+            }
+            else{
+                node = node.left;
+
+            }
+        }
+    }
+ }
 class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null);
 
-      public class BSTIterator{
-        private Stack<TreeNode> st = new Stack<>();
-        boolean reverse = true;
+        BSTIterator l = new BSTIterator(root,false);
+        BSTIterator r = new BSTIterator(root,true);
 
-        public BSTIterator(TreeNode root , boolean isReverse){
-            reverse = isReverse;
-            pushAll(root);
+        int i = l.next();
+        int j = r.next();
+
+        while(i < j){
+            if(i + j == k ) return true;
+            else if(i + j < k) i = l.next();
+            else j = r.next();
         }
+        return false;
 
-        public boolean hasNext(){
-            return !st.isEmpty();
-        }
-
-        public int next(){
-            TreeNode temp = st.pop();
-            if(reverse == false) pushAll(temp.right);
-            else pushAll(temp.left);
-            return temp.val;
-        }
-
-        private void pushAll(TreeNode node){
-            while(node != null){
-                st.push(node);
-                if(reverse == true){
-                    node = node.right;
-                }
-                else{
-                    node = node.left;
-                }
-            }
-        }
-      }
-        public boolean findTarget(TreeNode root, int k) {
-            if(root == null) return false;
-
-            BSTIterator l = new BSTIterator(root , false);
-            BSTIterator r = new BSTIterator(root , true);
-
-            int i = l.next();
-            int j = r.next();
-
-            while(i<j){
-                if(i + j == k) return true;
-                else if (i + j < k) i = l.next();
-                else j = r.next();
-            }
-            return false;
         
     }
 }
