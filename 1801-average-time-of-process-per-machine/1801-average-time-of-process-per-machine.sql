556c82fd0dbc6362cedae7c012891b9e2a0461d4
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-10-30 14:24:56 +0530

    Time: 234 ms (60.92%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -5,5 +5,5 @@ Activity as a2
 WHERE a1.activity_type = 'start' AND
 a2.activity_type = 'end' AND
 a1.process_id = a2.process_id
-AND a1.machine_id =a2.machine_id
+AND a1.machine_id = a2.machine_id
 GROUP BY machine_id;
