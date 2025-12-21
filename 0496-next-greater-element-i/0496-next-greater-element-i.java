dbf06404bc9fcf34a00fa86d04f890419739e1a4
Daksh Gaur <133903074+subliminalsublimity@users.noreply.github.com>
2025-12-21 06:51:19 +0530

    Time: 4 ms (65.32%), Space: 45.5 MB (14.59%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,23 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int [] ans = new int [nums1.length];
-        for(int i=0 ; i<nums1.length ; i++){
-            int element = nums1[i];
-            int nge = -1;
-            for(int j=0 ; j<nums2.length ; j++){
-                if(nums2[j] == element){
-                for(int k=j+1 ; k<nums2.length ; k++){
-                    if(nums2[k] > element){
-                        nge = nums2[k];
-                        break;
-                    }
-                }
-                break;
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=nums2.length - 1 ; i>=0;i--){
+            int curr = nums2[i];
+            while(!st.isEmpty() && st.peek() <= curr){
+                st.pop();
             }
+
+            int nge = st.isEmpty()?-1:st.peek();
+
+            mpp.put(curr , nge);
+            st.push(curr);
         }
-        ans[i] = nge;
+
+        int [] ans = new int [nums1.length];
+        for(int i=0 ; i<nums1.length;i++){
+           ans[i]=  mpp.get(nums1[i]);
         }
         return ans;
     }
-
 }
